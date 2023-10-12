4ed3aa6c5740f65d00aeb9c277ab7db9f430a5a5
Gorbun0v1 <joongleboo01@gmail.com>
2023-10-12 14:33:50 +0300

    HomeWork 2.12

--- HomeWork_2.3/src/test/java/com/example/demo/CalculatorServiceImplTestParametrist.java
+++ HomeWork_2.3/src/test/java/com/example/demo/CalculatorServiceImplTestParametrist.java
@@ -24,7 +24,7 @@ class CalculatorServiceImplTestParametrist {
 
     public static Stream<Arguments> testParamMinus() {
         return Stream.of(
-                Arguments.of(10, 7, "10 - 7 = 7."),
+                Arguments.of(10, 7, "10 - 7 = 3."),
                 Arguments.of(21, 5, "21 - 5 = 16."),
                 Arguments.of(30, 5, "30 - 5 = 25."),
                 Arguments.of(40, 5, "40 - 5 = 35.")
@@ -43,7 +43,7 @@ class CalculatorServiceImplTestParametrist {
     public static Stream<Arguments> testParamDivide() {
         return Stream.of(
                 Arguments.of(84, 7, "84 / 7 = 12."),
-                Arguments.of(21, 5, "20 / 5 = 4."),
+                Arguments.of(20, 5, "20 / 5 = 4."),
                 Arguments.of(30, 5, "30 / 5 = 6."),
                 Arguments.of(40, 5, "40 / 5 = 8.")
         );
@@ -59,21 +59,21 @@ class CalculatorServiceImplTestParametrist {
     @ParameterizedTest
     @MethodSource("testParamMinus")
     void minNumbers(Integer num1, Integer num2, String expectedLine) {
-        String actual = out.sumNumbers(num1, num2);
+        String actual = out.minNumbers(num1, num2);
         assertEquals(expectedLine, actual);
     }
 
     @ParameterizedTest
     @MethodSource("testParamMulti")
     void multiNumbers(Integer num1, Integer num2, String expectedLine) {
-        String actual = out.sumNumbers(num1, num2);
+        String actual = out.multiNumbers(num1, num2);
         assertEquals(expectedLine, actual);
     }
 
     @ParameterizedTest
     @MethodSource("testParamDivide")
     void divideNumbers(Integer num1, Integer num2, String expectedLine) {
-        String actual = out.sumNumbers(num1, num2);
+        String actual = out.divideNumbers(num1, num2);
         assertEquals(expectedLine, actual);
     }
 }
